0f80675419b1551a8d21a1ca0271d2f8f6b910d5
Rony Fernandez <rfernandezc123@gmail.com>
2024-11-16 23:09:08 -0500

    chore: Add AppRoutingModule and CommonsModule to app.module.ts

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren:()=> import('./welcome/welcome.module').then(m => m.WelcomeModule)
+  },
+  {
+    path:'courses',
+    loadChildren:()=> import('./courses/courses.module').then(m => m.CoursesModule)
+  }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CommonsModule } from './commons/commons.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    CommonsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
